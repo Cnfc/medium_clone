8db95e502dc5ecd832001805d89178caddd86744
Stanislav Dashkov <stdashkov@mail.ru>
2020-02-05 19:34:35 +0300

    add init auth pages

--- src/Routes.js
+++ src/Routes.js
@@ -3,11 +3,14 @@ import GlobalFeed from "./pages/globalFeed";
 import Article from "./pages/article";
 
 import { Switch, Route } from "react-router-dom";
+import { Auth } from "./pages/auth";
 
 export default () => {
   return (
     <Switch>
       <Route exact path="/" component={GlobalFeed} />
+      <Route path="/login" component={Auth} />
+      <Route path="/register" component={Auth} />
       <Route path="/articles/:slug" component={Article} />
     </Switch>
   );
